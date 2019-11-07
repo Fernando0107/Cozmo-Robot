36646f507a0763db6c0f528df159a23d9bf7f366
Diego Quan <diegoquan101@gmail.com>
2019-11-07 14:34:09 -0600

    Added base flask

--- Lex/lex.py
+++ Lex/lex.py
@@ -1,11 +1,19 @@
+import cozmo
 from flask import Flask
-
 app = Flask(__name__)
 
 
-@app.route("/")
-def index():
-    return "Hello from Flask!"
+
+def cozmo_program(robot: cozmo.robot.Robot):
+    robot.say_text("Hello Flask").wait_for_completed()
+
+
+@app.route('/')
+def hello_world():
+    cozmo.run_program(cozmo_program)
+
+    return 'Hello, World!'
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
