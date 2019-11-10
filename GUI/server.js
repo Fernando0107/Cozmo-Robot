29651d8674e1063a6a561b73a70b30a6ece6f831
Fernando González <rmfernando@outlook.com>
2019-11-10 16:13:20 -0600

    server.js and some folders added.

--- GUI/server.js
+++ GUI/server.js
@@ -1,7 +1,20 @@
 const express = require('express')
 const pug = require('pug')
 const app = express()
+var cons = require('consolidate');
+
+var bodyParser = require('body-parser');
 const port = 8080
+
+
+// view engine setup
+app.engine('html', cons.swig)
+app.use(express.static('views'))
+app.set('view engine', 'html');
+
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => res.send('Hello From Express'))
 
