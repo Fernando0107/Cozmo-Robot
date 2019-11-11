a219873403b9c4df412b151afd94de766d2ed904
Maite de la Roca <delaroca.estrada21@gmail.com>
2019-11-11 11:24:05 -0600

    cards for actions

--- GUI/app.js
+++ GUI/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const fs = require('fs');
 const app = express();
 const port = 8080
 
@@ -22,6 +23,11 @@ app.get('/todo', (req, res) => {
 
 });
 
+const todo = fs.readFileSync('./todo-cozmo.yml', 'utf8')
+
+app.get('/card', (req, res) => {
+    res.render('card');
+});
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}!`)
