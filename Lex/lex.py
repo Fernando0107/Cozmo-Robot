ec5afb3edc0788933d735e1a1ac58146a153fa30
BorisRendon <boris.rendon@ufm.edu>
2019-11-13 16:41:15 -0600

    Update de mostrar código transpilado en la página

--- Lex/lex.py
+++ Lex/lex.py
@@ -48,7 +48,7 @@ def hello_world():
     cozmo.run_program(cozmo_program)
 
     return 'Hello, World!'''
-codigo_generado = "import datetime \nimport cozmo\nclass transpiled:\n\n  def _init_(self, robot: cozmo.robot.Robot, cube: cozmo.objects.LightCube):\n      self.robot = robot\n      self.cube = cube\n\n  def cozmo_program(self, robot: cozmo.robot.Robot):\n      measure = cozmo.util\n+test+'\n  def run(self):\n      cozmo.run_program(self.cozmo_program)\nCOZMO = transpiled(cozmo.robot.Robot, cozmo.objects.LightCube)\nCOZMO.run()'"
+codigo_generado = "import datetime \nimport cozmo\nclass transpiled:\n\n  def _init_(self, robot: cozmo.robot.Robot, cube: cozmo.objects.LightCube):\n      self.robot = robot\n      self.cube = cube\n\n  def cozmo_program(self, robot: cozmo.robot.Robot):\n      measure = cozmo.util\n+test+'\n  def run(self):\n      cozmo.run_program(self.cozmo_program)\nCOZMO = transpiled(cozmo.robot.Robot, cozmo.objects.LightCube)\nCOZMO.run()"
 print(codigo_generado)
 @app.route('/root')
 def index():
